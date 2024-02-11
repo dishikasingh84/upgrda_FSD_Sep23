cb3aeabcbca86c0b9e1908978bc9c36127a28149
dishikasingh84 <dishikasingh84@gmail.com>
2024-02-11 21:15:22 +0530

    "Overloading main class"

--- src/com/company2/student.java
+++ src/com/company2/student.java
@@ -2,12 +2,8 @@ package com.company2;
 import java.io.IOException;
 import java.lang.*;
 
-class Adder{
-    static int add(int a, int b){return a+b;}
-    static double add(double a, double b){return a+b;}
+class TestOverloading4{
+    public static void main(String[] args){System.out.println("main with String[]");}
+    public static void main(String args){System.out.println("main with String");}
+    public static void main(){System.out.println("main without args");}
 }
-class TestOverloading2{
-    public static void main(String[] args){
-        System.out.println(Adder.add(11,11));
-        System.out.println(Adder.add(12.3,12.6));
-    }}
