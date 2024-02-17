3ed95bfbbca45439c84effd0d8d518305a377e9d
dishikasingh84 <dishikasingh84@gmail.com>
2024-02-17 20:20:06 +0530

    "Final Keyword"

--- src/com/company2/student.java
+++ src/com/company2/student.java
@@ -2,27 +2,11 @@ package com.company2;
 import java.io.IOException;
 import java.util.*;
 
-// Java program for the above approach
-import java.util.*;
-
-class Person{
-    int id;
-    String name;
-    Person(int id,String name){
-        this.id=id;
-        this.name=name;
+class Bike{
+    final void run(){System.out.println("running...");}
+}
+class Honda2 extends Bike{
+    public static void main(String args[]){
+        new Honda2().run();
     }
 }
-class Emp extends Person{
-    float salary;
-    Emp(int id,String name,float salary){
-        super(id,name);//reusing parent constructor
-        this.salary=salary;
-    }
-    void display(){System.out.println(id+" "+name+" "+salary);}
-}
-class TestSuper5{
-    public static void main(String[] args){
-        Emp e1=new Emp(1,"ankit",45000f);
-        e1.display();
-    }}
